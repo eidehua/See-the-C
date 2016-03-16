aa1ef89e091fd900b68a7a5791ef8520e1b6887b
Ed <eidehua@users.noreply.github.com>
2016-03-16 18:10:15 -0500

    Update custom-scheduler.c

--- Kernel/custom-scheduler.c
+++ Kernel/custom-scheduler.c
@@ -1,1 +1,4 @@
-Create own task struct, to build on top of Linux task struct & scheduler to implement our own rules.
+//Create own task struct, to build on top of Linux task struct & scheduler to implement our own rules.
+
+//Fixed priority (real time priority) a little confusing naming convention
+//Dynamic priority
