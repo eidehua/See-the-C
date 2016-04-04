4b6edc0f8e8a0083d63a59e759dcb265151df3d5
Ed <eidehua@users.noreply.github.com>
2016-04-03 20:32:45 -0500

    Create kernel-linked-list.c

---
+++ Kernel/kernel-linked-list.c
@@ -0,0 +1,11 @@
+//This is a circularly-linked list 
+
+//Usually a linked list contains the items that are to be linked. For example:
+struct my_list{
+	void *myitem;
+	struct my_list *next;
+	struct my_list *prev;
+	};
+	
+	
+//reference: https://isis.poly.edu/kulesh/stuff/src/klist/
