677bc05261b5b2b67bbe39ab7be52f42a3e550b8
Ed <eidehua@users.noreply.github.com>
2016-05-17 23:20:00 -0400

    Create pointers.c

---
+++ pointers.c
@@ -0,0 +1,5 @@
+//Understanding pointer logic in C
+//think of the name as the unique identifier (key) and the right hand side what the value is.
+int val = 20; //declaring an integer variable, (key = val, value = 20)
+int * ptr; //key = ptr, val = null
+ptr = &value; //key = ptr, val = address of (val)
