2e5321d53fde1ac489c9b71c0bc79a574672138c
Ed <eidehua@users.noreply.github.com>
2016-05-19 19:13:04 -0400

    Update pointers.c

--- pointers.c
+++ pointers.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 //Understanding pointer logic in C
 //think of the name as the unique identifier (key) and the right hand side what the value is.
 int val = 20; //declaring an integer variable, (key = val, value = 20)
@@ -6,6 +8,3 @@ ptr = &value; //key = ptr, val = address of (val)
 
 
 
-/*
- Function Pointers
-*/
